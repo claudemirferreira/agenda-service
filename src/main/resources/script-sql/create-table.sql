f23750a689621ae9bc81c8626d4912139217f190
Claudemir <claudemirramosferreira@gmail.com>
2023-12-03 19:32:29 -0300

    ajuste no script do database

--- src/main/resources/script-sql/create-table.sql
+++ src/main/resources/script-sql/create-table.sql
@@ -9,3 +9,7 @@ create table users (
 alter table users drop constraint UK_qfpm1fxdsr7178jkpqj6yusm5;
 
 alter table users add constraint UK_qfpm1fxdsr7178jkpqj6yusm5 unique (usu_login);
+
+create sequence s_usuario start with 1 increment by 1;
+
+insert into public.users values( 1 ,'admin@gmail.com','admin','Administrador do Sistema','$2a$10$lI1zzAi8XdDV7C4x0z9kc.CiWMGX3UkAe7vhmYPqZ//kt2K6bSzB.');
